6d681a0824e3efc1ad20c57396f0a7ccef41a1db
Aaron Scott <aaronjohnscott@gmail.com>
2023-04-24 09:15:09 -0700

    messing with fonts

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,15 @@
 import '@/styles/globals.css'
+import { Raleway } from 'next/font/google'
+
+const raleway = Raleway({
+  subsets: ['latin'],
+  weight: [ '700', '400'],
+})
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={raleway.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
